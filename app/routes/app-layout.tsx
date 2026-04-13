4379d28f777a241752015e5f999c4d4e8333c011
Terezie <terezie.prchalova1@gmail.com>
2026-04-13 10:29:47 +0200

    Add results component to the layout

--- app/routes/app-layout.tsx
+++ app/routes/app-layout.tsx
@@ -1,13 +1,26 @@
-import { Outlet } from "react-router"
+import { Outlet, useLoaderData } from "react-router"
 import { useState } from "react";
 import Sidebar from "~/components/Sidebar"
+import Results from "~/components/Results";
+import { getEntries } from "~/lib/entries.server";
+
+export async function loader() {
+  const entries = await getEntries();
+  return { entries };
+}
+
 
 export default function AppLayout() {
+    const { entries } = useLoaderData<typeof loader>();
     const [isPanelOpen, setIsPanelOpen] = useState(false);
+
     return (
         <div className="flex h-screen w-screen">
             <Sidebar />
             <Outlet/>
+            <div className="hidden w-full h-full lg:block">
+                <Results entries={entries} />
+            </div>
         </div>
     )
 }
