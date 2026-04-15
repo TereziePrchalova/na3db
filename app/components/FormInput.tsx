2e50f249fa0d2a2ae51f12a9bf0f4118a0bbf07d
Terezie <terezie.prchalova1@gmail.com>
2026-04-15 15:24:59 +0200

    Update FormInput

--- app/components/FormInput.tsx
+++ app/components/FormInput.tsx
@@ -1,17 +1,23 @@
 type FormInputProps = {
-    label: string;
+    label?: string;
+    type?: string;
     placeholder?: string;
+    value: string;
+    setValue: (value: string) => void;
 };
 
-export default function FormInput({ label, placeholder }: FormInputProps) {
+export default function FormInput({ label, placeholder, value ,setValue, type }: FormInputProps) {
     return (
-        <div className="mb-4">
+        <div className="mb-4 w-full">
             <div className="font-medium text-xs mb-1 pl-1">
                 {label}
             </div>
             <input
-                placeholder={placeholder}
                 className="bg-transparent border border-[#4A4A46] p-2 rounded-[5px] w-full text-xs"
+                type={type || "text"}
+                placeholder={placeholder}
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
             />
         </div>
     );
