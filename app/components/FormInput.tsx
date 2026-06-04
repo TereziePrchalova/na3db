d06faa578b9749814bda8dfc8e32a60af4c290ee
Terezie <terezie.prchalova1@gmail.com>
2026-06-04 16:13:29 +0200

    Improve input border styles

--- app/components/FormInput.tsx
+++ app/components/FormInput.tsx
@@ -14,7 +14,7 @@ export default function FormInput({ label, placeholder, type, registration }: Fo
                 {label}
             </div>
             <input
-                className="bg-transparent border border-[#4A4A46] p-2 rounded-[5px] w-full text-xs"
+                className="bg-transparent border border-[#4A4A46] p-2 rounded-[5px] w-full text-xs outline-none focus:border-[#1D9E75] hover:border-[#6A6A66] transition-all"
                 type={type || "text"}
                 placeholder={placeholder}
                 {...registration}
